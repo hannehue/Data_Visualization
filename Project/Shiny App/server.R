3ad06268a7bccedca9ee7470282c022947beb9c0
Jesper1998 <71690231+Jesper1998@users.noreply.github.com>
2022-11-22 16:11:29 +0100

    stash#

--- Project/Shiny App/server.R
+++ Project/Shiny App/server.R
@@ -1,12 +1,15 @@
 shinyServer(
   function(input, output, session) {
     
+    
+    
+  
+  # Barchart
   age = raw_data$age3
   mascu = raw_data$q0002
   xlist = list(age, mascu)
   mascu_df = as.data.frame(xlist)
-
-
+  
   output$mascuPlot <- renderPlot({
     mascu_df %>%
       count(age, mascu = factor(mascu)) %>%
@@ -15,11 +18,5 @@ shinyServer(
       ggplot(aes(age, n, fill = mascu)) +
       geom_col(position = "dodge")
   }, height = 600, width = 900)
-  
-  #Masculinity_Data = read_csv("masculinity-survey.csv")
-  #raw_data = read_csv("raw-responses.csv")
-
-  
-
 
 })

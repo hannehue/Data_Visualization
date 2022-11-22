67974b52885923094856632f0090fff05de31c52
Jesper1998 <71690231+Jesper1998@users.noreply.github.com>
2022-11-22 16:06:16 +0100

    working chart

--- Project/Shiny App/server.R
+++ Project/Shiny App/server.R
@@ -2,16 +2,19 @@ shinyServer(
   function(input, output, session) {
     
   age = raw_data$age3
-  mascu = raw_data$q0001
+  mascu = raw_data$q0002
   xlist = list(age, mascu)
   mascu_df = as.data.frame(xlist)
 
 
   output$mascuPlot <- renderPlot({
-    
-    ggplot(data = mascu_df, aes(x = age, y = percent , fill=mascu)) +
-      geom_bar(aes(y = (..count..)/sum(..count..)), position = position_dodge())
-  }, height = 600, width = 600)
+    mascu_df %>%
+      count(age, mascu = factor(mascu)) %>%
+      group_by(age) %>%
+      mutate(n = prop.table(n) * 100) %>%
+      ggplot(aes(age, n, fill = mascu)) +
+      geom_col(position = "dodge")
+  }, height = 600, width = 900)
   
   #Masculinity_Data = read_csv("masculinity-survey.csv")
   #raw_data = read_csv("raw-responses.csv")

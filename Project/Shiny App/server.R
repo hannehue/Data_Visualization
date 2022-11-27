b2e8e0e3a427126a591482d6dc9f43482c32dae1
Jesper1998 <71690231+Jesper1998@users.noreply.github.com>
2022-11-27 18:00:11 +0100

    bar stacked chart

--- Project/Shiny App/server.R
+++ Project/Shiny App/server.R
@@ -1,25 +1,25 @@
 shinyServer(
   function(input, output, session) {
     
-#     barchart = reactive({
-#       selected.quiestion = input$quiestion_picker
-#       age = raw_data$age3
-#       mascu = raw_data$selected.quiestion
-#       xlist = list(age, mascu)
-#       mascu_df = as.data.frame(xlist)
-#       
-#       mascu_df %>%
-#         count(age, mascu = factor(mascu)) %>%
-#         group_by(age) %>%
-#         mutate(n = prop.table(n) * 100) %>%
-#         ggplot(aes(age, n, fill = mascu)) +
-#         geom_col(position = "dodge")
-#     }, height = 600, width = 900)
-#   })
-# 
-# output$mascuPlot <- renderPlot({
-#   barchart
-# })
+    #     barchart = reactive({
+    #       selected.quiestion = input$quiestion_picker
+    #       age = raw_data$age3
+    #       mascu = raw_data$selected.quiestion
+    #       xlist = list(age, mascu)
+    #       mascu_df = as.data.frame(xlist)
+    #       
+    #       mascu_df %>%
+    #         count(age, mascu = factor(mascu)) %>%
+    #         group_by(age) %>%
+    #         mutate(n = prop.table(n) * 100) %>%
+    #         ggplot(aes(age, n, fill = mascu)) +
+    #         geom_col(position = "dodge")
+    #     }, height = 600, width = 900)
+    #   })
+    # 
+    # output$mascuPlot <- renderPlot({
+    #   barchart
+    # })
     
     observe({
       print(input$quiestion_picker)
@@ -44,7 +44,22 @@ shinyServer(
       
     })
     
-  
-
-
-})
+    
+    dat_sub <- raw_data %>%
+      select(age3, racethn4) %>%
+      group_by(age3, racethn4) %>%
+      summarise(n = n()) %>%
+      group_by(age3) %>%
+      mutate(tot = sum(n), perc = n / tot * 100)
+    
+    output$stacked_bar_chart <- renderPlot({
+      ggplot(dat_sub, aes(x = age3, y = perc, fill = racethn4)) +
+        geom_col(width = 1) +
+        ggtitle("Age and race distribution of respondees") +
+        xlab("Age groups") +
+        ylab("Respondee percentage") +
+        scale_fill_discrete(name = "Race")
+      
+    })
+    
+  })

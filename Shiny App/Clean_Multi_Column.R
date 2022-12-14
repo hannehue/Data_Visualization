c3312ca9f6f424f4f9198745d028b11a4536d7d3
hannehue <Hannehue@gmail.com>
2022-12-14 20:18:12 +0100

    Working Multicolumn pretty. Fuck yes ahah

---
+++ Shiny App/Clean_Multi_Column.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+Test <- read_excel("Shiny App/data_files/Clean_Multi_Column.xlsx")
+
+Test <- Test %>%
+  group_by(Question) %>%
+  mutate(percentage = Value / sum(Value) * 100)
+
+Test$Selection = as.factor(Test$Selection)
+Test$Selection = factor(Test$Selection, levels = c("Selected", "Not Selected"))
+Test$percentage = round(Test$percentage, 0)
+Test$Question = factor(Test$Question, levels = c("None of the above", "Height", "Appearance of genetalia", "Clothing style", "Hair", "Sexual performance or amount of sex",
+                                                 "Mental health", "Ability to provide for family", "Physique", "Your finances", "Physical health",
+                                                 "Weight"))
+ggplot(Test, aes(fill = Selection, x = Question, y = percentage)) +
+  geom_bar(position = "fill", stat = "identity") +
+  coord_flip() + scale_y_reverse() +
+  labs(title = "What do you worry about on a daily/near daily basis")
+
+# SAVE THIS FUCKING LINK
+# https://gist.github.com/ale-lazic/f9754aff54acfcc0e32718d5685817c3
--- Shiny App/multicolumn.R
+++ Shiny App/multicolumn.R
@@ -18,26 +18,51 @@ questions = c(
 
 
 q0801Count = factor(worry_about$q0008_0001)
+cadkj = count(q0801Count)
+table(q0801Count)
+table(q0801Count)
+
+ggplot()
+
+worry = formatted_data[, c(
+  "q0008_0001",
+  "q0008_0002",
+  "q0008_0003",
+  "q0008_0004",
+  "q0008_0005",
+  "q0008_0006",
+  "q0008_0007",
+  "q0008_0008",
+  "q0008_0009",
+  "q0008_0010",
+  "q0008_0011",
+  "q0008_0012"
+)]
+
+worry <- as.data.frame(worry)
+colnames(worry) <- questions
+library(reshape2)
+aaa <- melt(worry)
+view(worry)
+
+
+
+
 
 ggplot(data = worry_about) +
-  geom_bar(aes(x = questions[1], fill = q0008_0001)) +
-  geom_bar(aes(x = questions[2], fill = q0008_0002)) +
-  geom_bar(aes(x = questions[3], fill = q0008_0003)) +
-  geom_bar(aes(x = questions[4], fill = q0008_0004)) + 
-  geom_bar(aes(x = questions[5], fill = q0008_0005)) +
-  geom_bar(aes(x = questions[6], fill = q0008_0006)) + 
-  geom_bar(aes(x = questions[7], fill = q0008_0007)) +
-  geom_bar(aes(x = questions[8], fill = q0008_0008)) + 
-  geom_bar(aes(x = questions[9], fill = q0008_0009)) +
-  geom_bar(aes(x = questions[10], fill = q0008_0010)) + 
-  geom_bar(aes(x = questions[11], fill = q0008_0011)) +
-  geom_bar(aes(x = questions[12], fill = q0008_0012)) + 
+  geom_bar(aes(x = questions[1], fill = q0008_0001), position = "fill") +
+  geom_bar(aes(x = questions[2], fill = q0008_0002), position = "fill") +
+  geom_bar(aes(x = questions[3], fill = q0008_0003), position = "fill") +
+  geom_bar(aes(x = questions[4], fill = q0008_0004), position = "fill") + 
+  geom_bar(aes(x = questions[5], fill = q0008_0005), position = "fill") +
+  geom_bar(aes(x = questions[6], fill = q0008_0006), position = "fill") + 
+  geom_bar(aes(x = questions[7], fill = q0008_0007), position = "fill") +
+  geom_bar(aes(x = questions[8], fill = q0008_0008), position = "fill") + 
+  geom_bar(aes(x = questions[9], fill = q0008_0009), position = "fill") +
+  geom_bar(aes(x = questions[10], fill = q0008_0010), position = "fill") + 
+  geom_bar(aes(x = questions[11], fill = q0008_0011), position = "fill") +
+  geom_bar(aes(x = questions[12], fill = q0008_0012), position = "fill") + 
   coord_flip()
 
-mascu_df %>%
-  count(age, quest) %>%
-  group_by(age) %>%
-  mutate(n = prop.table(n) * 100) %>%
-  ggplot(aes(age, n, fill = quest)) +
-  geom_col(position = "dodge")
-}, height = 600, width = 900)
+view(q0801Count)
+view(worry_about$q0008_0001)
